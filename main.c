7fa399ea5c7d3ab8c0ff64f0c1af313b2c5ad834
Erick Banhos <erick.bcastro99@gmail.com>
2026-03-18 14:18:34 -0300

    Adicionando projeto ao git

---
+++ main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main() {
+    char nome[50];
+    int (idade);
+    printf("Bem-vindo ao programa de cadastro!\n");
+    sleep (3);
+    system("cls");
+
+    printf ("Para acessar um cadastro existente digite 0, para criar um novo cadastro digite 1: ");
+    int escolha;
+    scanf("%d", &escolha);
+    if (escolha ==0) {
+        printf("%s, %d anos", nome, idade);
+    }
+    if (escolha == 1) {
+        printf("Criando um novo cadastro...\n");
+        sleep (2);
+        system("cls");
+    }
+    printf("Digite seu nome: ");
+    scanf("%s", nome);
+    system("cls");
+
+    printf("Ola, %s!\n", nome);
+    sleep (1);
+    system("cls");
+
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+    system("cls");
+
+    printf("Cadastro completo!\n");
+    sleep (2);
+    system("cls");
+    
+    printf("Se voce quiser ver e/ou editar seus dados, digite 1. Caso contrario, digite 0: ");
+    int opcao;
+    scanf("%d", &opcao);
+    if (opcao == 1) {
+        printf("Nome: %s", nome);
+        printf("Idade: %d\n", idade);
+        sleep (2);
+        system("cls");
+        }
+        printf("Deseja editar seus dados? Digite 1 para sim ou 0 para nao: ");
+        int editar;
+        scanf("%d", &editar);
+
+        if (editar == 1) {
+            printf("Digite seu novo nome: ");
+            scanf("%s", nome);
+            system("cls");
+
+            printf("Digite sua nova idade: ");
+            scanf("%d", &idade);
+            system("cls");
+
+            printf("Dados atualizados!\n");
+            sleep (2);
+            system("cls");
+        }
+    printf("Obrigado por usar o programa de cadastro, %s!\n", nome);
+    sleep (2);
+    return 0;
+}
